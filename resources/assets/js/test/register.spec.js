0c802deb7926e2f21358e6b92b3494ceab8a7f3b
Monika Marusiak <moniusiar@gmail.com>
2020-05-23 13:45:46 +0200

    tests working with localStorage :D

--- resources/assets/js/test/register.spec.js
+++ resources/assets/js/test/register.spec.js
@@ -2,8 +2,8 @@ import { mount, createLocalVue } from 'vue-test-utils';
 import expect from 'expect';
 
 import Register from "../components/Register.vue";
-// import Login from "../components/Login.vue";
-// import App from "../App.vue";
+import Login from "../components/Login.vue";
+import App from "../App.vue";
 import Home from "../components/Home.vue";
 
 import axios from 'axios';
@@ -17,8 +17,8 @@ const localVue = createLocalVue()
 // localVue.use(VueRouter)
 localVue.use(VueAxios, axios);
 
-describe('Home', () => {
-    let wrapper = mount(Home);
+describe('Login', () => {
+    let wrapper = mount(Login);
     it('shows welcoming text', () => {
         expect(wrapper.html()).toContain("Witamy w aplikacji Katolickiego Stowarzyszenia Młodzieży Diecezji Legnickiej")
     })
