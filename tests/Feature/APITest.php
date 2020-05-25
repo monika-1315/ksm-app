b5f39ba8d70fdc27ece5c8fdc5ad95182badff4c
Monika Marusiak <moniusiar@gmail.com>
2020-05-25 12:20:21 +0200

    auth tests

--- tests/Feature/APITest.php
+++ tests/Feature/APITest.php
@@ -5,38 +5,34 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\User;
-use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class APITest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testGetDivisions()
     {
         $response = $this->get('/api/getDivisions');
         // $response->dump();
         $response->assertStatus(200);
         $response->assertJsonStructure([
-                '*' => [
+            '*' => [
                 'id', 'is_active', 'parish', 'town'
-                ]
-        ]);        
+            ]
+        ]);
     }
 
     public function testGetDivisionsById()
     {
+
         $response = $this->get('/api/getDivisionById?id=1');
         // $response->dump();
         $response->assertStatus(200)
             ->assertJsonStructure([
                 '*' => [
-                'id', 'is_active', 'parish', 'town'
+                    'id', 'is_active', 'parish', 'town'
                 ]
-        ])
-            ->assertJsonPath('0.id', '1');        
+            ])
+            ->assertJsonPath('0.id', '1');
     }
 
     public function testGetAllDivisions()
@@ -44,47 +40,15 @@ class APITest extends TestCase
         $response = $this->get('/api/auth/allDivisions');
         $response->assertStatus(405);
 
-        $user = User::first();       
-        $token = \JWTAuth::fromUser($user); 
-        $response= $this->json('POST', '/api/auth/allDivisions', ['token' => $token]);
+        $user = User::first();
+        $token = JWTAuth::fromUser($user);
+        $response = $this->json('POST', '/api/auth/allDivisions', ['token' =>$token]);
         // $response->dump();
         $response->assertStatus(200);
         $response->assertJsonStructure([
-                '*' => [
+            '*' => [
                 'id', 'is_active', 'parish', 'town'
-                ]
-        ]);        
+            ]
+        ]);
     }
-
-    public function testLogin()
-{
-
-    $response = $this->json('POST', '/api/auth/login', [
-        'email' => 'moniusiar@gmail.com',
-        'password' => 'password'
-    ]);
-
-    $response
-        // ->dump()
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            'access_token', 'token_type', 'user'
-        ]);
-}
-
-public function testLogout()
-{
-    $user = User::first();
-    $token = \JWTAuth::fromUser($user);
-
-    $response = $this->json('GET', '/api/auth/logout?token=' . $token, []);
-
-    $response
-    // ->dump()
-        ->assertStatus(200)
-        ->assertExactJson([
-            'success' => true
-        ]);
-}
-
 }
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthTest extends TestCase
+{
+    public function testLogin()
+    {
+
+        $response = $this->json('POST', '/api/auth/login', [
+            'email' => 'moniusiar@gmail.com',
+            'password' => 'password'
+        ]);
+
+        $response
+            // ->dump()
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token', 'token_type', 'user'
+            ]);
+    }
+
+    public function testLogout()
+    {
+        $user = User::first();
+        $token = JWTAuth::fromUser($user);
+
+        $response = $this->json('GET', '/api/auth/logout?token=' . $token, []);
+
+        $response
+            // ->dump()
+            ->assertStatus(200)
+            ->assertExactJson([
+                'success' => true
+            ]);
+    }
+
+    public function testRegister()
+    {
+        
+        $response = $this->json('POST', '/api/auth/register', [
+            'name'=> 'new neme',
+            'division'=> 1
+        ]);
+
+        $response
+            ->dump()
+            ->assertStatus(422)
+            ->assertExactJson([
+                'errors'=>[
+                    'birthdate'=>['Pole birthdate jest wymagane.'],
+                    'confirmPassword'=>['Pole confirm password jest wymagane.'],
+                    'email'=>['Pole email jest wymagane.'],
+                    'password'=>['Pole password jest wymagane.'],
+                    'surname'=>['Pole surname jest wymagane.']
+                ],
+                'message'=>"The given data was invalid."
+                    
+            ]);
+    }
+
+   
+}
