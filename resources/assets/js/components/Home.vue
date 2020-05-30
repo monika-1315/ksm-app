99a0ec135464ee86cb9c168a029c734e199c73b6
Monika Marusiak <moniusiar@gmail.com>
2020-05-30 21:37:06 +0200

    better home page

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,15 +1,46 @@
 <template>
-   <div id="app">
-            <img alt="KSM logo" src="./assets/logo.png">
-            <br><br>
-            <div style="padding 20px" >
-                <center><h4>Witamy w aplikacji Katolickiego Stowarzyszenia Młodzieży Diecezji Legnickiej</h4></center>
-            </div>
+  <div id="app">
+      <br>
+      <div >
+      <center>
+        <h4>Witamy w aplikacji <br>Katolickiego Stowarzyszenia Młodzieży <br>Diecezji Legnickiej</h4>
+      </center> <br />
+    <img alt="KSM logo" src="./assets/logo.png" />
+    <br />
+   
+    
     </div>
+    <br />
+    <h6>
+      Zaloguj się, aby uzyskać dostęp do tablicy ogłoszeń.
+      <br />
+    </h6>
+     <div>
+      <router-link id="reg" :to="{ name: 'login' }" class="nav-link">
+        <button class="btn btn-primary">Zaloguj się</button>
+      </router-link>
+    </div>
+    <h6>
+      Nie masz konta?
+      <br />
+    </h6>
+    <div>
+      <router-link id="reg" :to="{ name: 'register' }" class="nav-link">
+        <button class="btn btn-primary">Zarejestruj się</button>
+      </router-link>
+    </div>
+    <h5>
+      <br />Czym jest Katolickie Stowarzysznie Młodzieży? Zobacz na naszej
+      <a href="http://ksm.legnica.pl">stronie! -></a>
+    </h5>
+  </div>
 </template>
 
 <style scoped>
-    #app{
-        text-align: center;
-    }
+#app {
+  text-align: center;
+}
+.center{
+    padding: 10%;
+}
 </style>
