2a2b7ad5b32918854f3a45a1484b917335c35124
Monika Marusiak <moniusiar@gmail.com>
2020-05-31 20:30:14 +0200

    checking login and log out

--- tests/acceptance/LoggedMessagesCept.php
+++ tests/acceptance/LoggedMessagesCept.php
@@ -5,6 +5,10 @@ $I->am('user'); // actor's role
 $I->wantTo('log in'); // feature to test
 $I->amOnPage('/login');
 $I->see('Zaloguj się');
+$I->click('#log-in');
+$I->waitForElement('.text-danger',15);
+$I->see('Pole email jest wymagane.');
+$I->see('Pole password jest wymagane.');
 $I->fillField('#email','moniusiar@gmail.com');
 $I->fillField('#password','password');
 $I->click('#log-in');
@@ -22,6 +26,7 @@ $I->click('Wszystkie');
 $I->wait(1);
 $I->see('Witaj Monika');
 
+$I->wantTo('log out');
 $I->click('Wyloguj się');
 $I->waitForElement('#email',30);
 $I->see('Zaloguj się');
