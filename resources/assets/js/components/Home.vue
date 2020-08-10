bc56347d0bdece5b9db445325fcf283aff89da62
Monika Kucharska <moniusiar@gmail.com>
2020-08-10 21:45:48 +0200

    improved viewing home for logged users

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,46 +1,79 @@
 <template>
   <div id="app">
-      <br>
-      <div >
-      <center>
-        <h4>Witamy w aplikacji <br>Katolickiego Stowarzyszenia Młodzieży <br>Diecezji Legnickiej</h4>
-      </center> <br />
-    <img alt="KSM logo" src="./assets/logo.png" />
     <br />
-   
-    
-    </div>
-    <br />
-    <h6>
-      Zaloguj się, aby uzyskać dostęp do tablicy ogłoszeń.
-      <br />
-    </h6>
-     <div>
-      <router-link id="reg" :to="{ name: 'login' }" class="nav-link">
-        <button class="btn btn-primary">Zaloguj się</button>
-      </router-link>
-    </div>
-    <h6>
-      Nie masz konta?
-      <br />
-    </h6>
     <div>
-      <router-link id="reg" :to="{ name: 'register' }" class="nav-link">
-        <button class="btn btn-primary">Zarejestruj się</button>
-      </router-link>
+      <center>
+        <h4>
+          Witamy w aplikacji
+          <br />Katolickiego Stowarzyszenia Młodzieży
+          <br />Diecezji Legnickiej
+        </h4>
+      </center>
+      <br />
+      <img alt="KSM logo" src="./assets/logo.png" />
+      <br />
     </div>
-    <h5>
-      <br />Czym jest Katolickie Stowarzysznie Młodzieży? Zobacz na naszej
-      <a href="http://ksm.legnica.pl">stronie! -></a>
-    </h5>
+    <br />
+    <div v-if="!this.$store.state.isLoggedIn">
+      <h6>
+        Zaloguj się, aby uzyskać dostęp do tablicy ogłoszeń.
+        <br />
+      </h6>
+      <div>
+        <router-link id="reg" :to="{ name: 'login' }" class="nav-link">
+          <button class="btn btn-primary">Zaloguj się</button>
+        </router-link>
+      </div>
+      <h6>
+        Nie masz konta?
+        <br />
+      </h6>
+      <div>
+        <router-link id="reg" :to="{ name: 'register' }" class="nav-link">
+          <button class="btn btn-primary">Zarejestruj się</button>
+        </router-link>
+      </div>
+      <h5>
+        <br />Czym jest Katolickie Stowarzysznie Młodzieży? Zobacz na naszej
+        <a
+          href="http://ksm.legnica.pl"
+        >stronie! -></a>
+      </h5>
+    </div>
+    <div v-else>
+      <router-link id="dash" :to="{ name: 'dashboard' }" class="nav-link">
+          <button class="btn btn-large btn-primary">Wiadomości</button>
+        </router-link>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    is_logged() {
+      return this.$store.state.isLoggedIn;
+    },
+  },
+  watch: {
+    is_logged: function () {
+      if (is_logged === true) this.$router.push("dashboard");
+    },
+  },
+  methods: {
+    created: function () {
+      // if (is_logged===true)
+      this.$router.push({ name: "login" });
+    },
+  },
+};
+</script>
 
 <style scoped>
 #app {
   text-align: center;
 }
-.center{
-    padding: 10%;
+.center {
+  padding: 10%;
 }
 </style>
