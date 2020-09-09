40a2f0c4b72fe853fcbf5f43bca63f89266660b9
Monika Kucharska <moniusiar@gmail.com>
2020-09-09 18:40:02 +0200

    added manifest (for PWA)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,17 @@
 <html>
 <head>
     <meta charset="utf-8">
+    
+    <link rel="manifest" crossorigin="use-credentials" href="./manifest.json">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#fdd835">
     <title>KSM DL</title>
 
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
    
 </head>
 <body >
     <div id="app"></div>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
 <script src="/js/app.js"></script>
 </body>
 </html>
