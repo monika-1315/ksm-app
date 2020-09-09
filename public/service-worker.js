9e5ee0915773d19ca4c4cdab4472ee468dce5b66
Monika Kucharska <moniusiar@gmail.com>
2020-09-09 19:35:52 +0200

    added service worker for PWA

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+const MY_CACHE = 'my-cache-name';
+// w tej tablicy lądują wszystkie pliki, które chcemy dodać do cache
+const MY_FILES = [
+        '/css/app.css',
+        '/images/logo.png',
+        '/js/app.js',
+        '/images/logo.png?9a5e800bbc300cb2cb86ef1a5b0af43d',
+        '/manifest.json'
+];
+
+// instalujemy nasz service worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(MY_CACHE).then(function(cache) {
+      return cache.addAll(MY_FILES);
+    })
+  );
+});
+// po aktywacji chcę skasować wszystkie cache w naszej domenie, które nie są naszym cache (to opcjonalne)
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== MY_CACHE;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+// strategia 'Network falling back to cache'
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
     <meta charset="utf-8">
     
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#fdd835">
+    <meta name="description" content="Aplikacja Katolickiego Stowarzyszenia Młodzieży Legnickiej">
     <title>KSM DL</title>
 
    
@@ -14,5 +15,17 @@
 <body >
     <div id="app"></div>
 <script src="/js/app.js"></script>
+<script>
+// sprawdzamy czy przeglądarka posiada wsparcie dla service workera
+if ('serviceWorker' in navigator) {
+    // próba instalacji
+    navigator.serviceWorker.register('/service-worker.js').then(function () {
+        console.log('Service worker zainstalowany');
+    }).catch(function (err) {
+        // jeśli coś pójdzie nie tak- konsola nam powie co trzeba poprawić
+        console.log('Service worker nie zainstalowany, sprawdź błąd:', err)
+    });
+}
+</script>
 </body>
 </html>
